5db637144afa3d7a31da3d0d16b14da2b7780c04
Nathan Nette <23629988+NerbzHub@users.noreply.github.com>
2026-07-17 22:29:48 +1000

    Sprint B03-S038
    Implement Error Reporting
    
    - completed sprint objective
    - validation completed

--- builder/book/__init__.py
+++ builder/book/__init__.py
@@ -4,6 +4,12 @@ from .loading import (
     LoadedBook,
     load_book_document,
 )
+from .issues import (
+    ParsingIssue,
+    ParsingIssueReport,
+    ReportParsingIssuesJob,
+    report_parsing_issues,
+)
 from .dependencies import (
     ConceptualDependency,
     DependencyRecognitionError,
@@ -57,16 +63,20 @@ __all__ = [
     "GenerateParsingReportJob",
     "LoadBookDocumentJob",
     "LoadedBook",
+    "ParsingIssue",
+    "ParsingIssueReport",
     "ParseDocumentStructureJob",
     "ParsingReport",
     "RecogniseBooksJob",
     "RecogniseDependenciesJob",
+    "ReportParsingIssuesJob",
     "StructureParsingError",
     "SprintDiscoveryError",
     "load_book_document",
     "generate_parsing_report",
     "determine_construction_progress",
     "recognise_conceptual_dependencies",
+    "report_parsing_issues",
     "parse_document_structure",
     "discover_construction_sprints",
     "recognise_engineering_books",
