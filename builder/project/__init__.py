a7cf2c2d708829932beef1ad1502e46cd0017368
Nathan Nette <23629988+NerbzHub@users.noreply.github.com>
2026-07-17 22:00:05 +1000

    Sprint B03-S024
    Implement Project Overview
    
    - completed sprint objective
    - validation completed

--- builder/project/__init__.py
+++ builder/project/__init__.py
@@ -9,6 +9,11 @@ from .loading import (
     ProjectMetadata,
     load_project_metadata,
 )
+from .overview import (
+    GenerateProjectOverviewJob,
+    ProjectOverview,
+    generate_project_overview,
+)
 from .registration import (
     PROJECT_REGISTRATION_FILENAME,
     ProjectRegistration,
@@ -21,16 +26,19 @@ from .registration import (
 
 __all__ = [
     "DiscoverProjectsJob",
+    "GenerateProjectOverviewJob",
     "LoadProjectJob",
     "PROJECT_REGISTRATION_FILENAME",
     "ProjectDiscoveryError",
     "ProjectLoadingError",
     "ProjectMetadata",
+    "ProjectOverview",
     "ProjectRegistration",
     "ProjectRegistrationError",
     "RecogniseProjectJob",
     "RegisterProjectJob",
     "discover_projects",
+    "generate_project_overview",
     "load_project_metadata",
     "recognise_project",
     "register_project",
