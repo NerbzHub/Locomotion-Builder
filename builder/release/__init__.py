15f3be7ff64df827f273b8d531ea152d659ecb8d
Nathan Nette <23629988+NerbzHub@users.noreply.github.com>
2026-07-17 23:40:05 +1000

    Verify Version 1 readiness

--- builder/release/__init__.py
+++ builder/release/__init__.py
@@ -6,10 +6,18 @@ from .preparation import (
     ReleasePreparation,
     prepare_release,
 )
+from .verification import (
+    FinalVerification,
+    VerifyVersionOneReadinessJob,
+    verify_version_one_readiness,
+)
 
 __all__ = [
     "PrepareReleaseJob",
+    "FinalVerification",
     "RELEASE_VERSION",
     "ReleasePreparation",
+    "VerifyVersionOneReadinessJob",
     "prepare_release",
+    "verify_version_one_readiness",
 ]
